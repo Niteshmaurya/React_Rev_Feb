fdec47b852ab618dceaeee60a91c4baf5d2f5e32
Niteshmaurya <104177011+Niteshmaurya@users.noreply.github.com>
2024-03-05 10:01:20 +0530

    added production level component and learnt about Forward Refrencing which is written on blog till lect 23 on 5 march 2024

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,40 @@
 import './App.css' 
+import React ,{useState, useEffect} from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from "./Store/authSlice"
+// import {Header} from './components/Header/Header'
+// import {Footer} from './components/Footer/Footer'
+import { Header,Footer } from './components'
 
 function App() {
-  console.log(import.meta .env.VITE_APPWRITE_URL)
+  const [loading, setloading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-  <>
-  <h1>A blog app with appwrite</h1>
-  </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setloading(false))
+  },[])
+
+  return !loading? (
+    <div className='min-h-sc flex content-between flex-wrap bg-grey-800'> 
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        TODO: {/* <outlet/> */}
+      </main>
+      <Footer/>
+      </div>
+      </div>
+  ) : null
 }
 
 export default App
